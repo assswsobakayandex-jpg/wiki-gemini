0baa07bad8fe26c2a145ca22c87f09a9a27e7027
assswsobakayandex-jpg <assswsobakayandex@gmail.com>
2026-03-19 15:38:32 +0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -31,7 +31,7 @@ module.exports = async (req, res) => {
 
     // Отправляем запрос к Gemini
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${apiKey}`,
+     `https://generativelanguage.googleapis.com/v1/models/gemini-1.0-pro:generateContent?key=${apiKey}`,
       {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
