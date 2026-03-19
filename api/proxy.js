f0f97e910d5a08113849058cf38c8cf8a9329c1f
assswsobakayandex-jpg <assswsobakayandex@gmail.com>
2026-03-19 18:14:47 +0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,1 +1,11 @@
+module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
+  if (req.method === 'OPTIONS') {
+    return res.status(204).end();
+  }
+
+  res.status(200).json({ message: 'Proxy работает!' });
+};
